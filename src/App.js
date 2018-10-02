73d82b1ecbb0c7c03afdab9d7226c619e1ad2b87
vartanbeno <vartanbeno@gmail.com>
2018-10-02 19:51:40 -0400

    stateless component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Ninjas from './Ninjas';
 
 class App extends Component {
-  
+
   state = {
     ninjas: [
       { id: 1, name: 'Vartan', age: 23, belt: 'black' },
--- src/Ninjas.js
+++ src/Ninjas.js
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Ninjas extends Component {
-    render() {
-        const { ninjas } = this.props;
+const Ninjas = (props) => {
+    const { ninjas } = props;
 
-        const listOfNinjas = ninjas.map(ninja => {
-            return (
-                <div className="ninja" key={ ninja.id }>
-                    <div>Name: { ninja.name }</div>
-                    <div>Age: { ninja.age }</div>
-                    <div>Belt: { ninja.belt }</div>
-                </div>
-            )
-        })
-
+    const listOfNinjas = ninjas.map(ninja => {
         return (
-            <div className="ninja-list">
-                { listOfNinjas }
+            <div className="ninja" key={ ninja.id }>
+                <div>Name: { ninja.name }</div>
+                <div>Age: { ninja.age }</div>
+                <div>Belt: { ninja.belt }</div>
             </div>
-        );
-    }
+        )
+    })
+
+    return (
+        <div className="ninja-list">
+            { listOfNinjas }
+        </div>
+    );
 }
 
 export default Ninjas;
