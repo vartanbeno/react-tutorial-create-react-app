03a0f2c407ea3fafd97cb26e2f84d1d0e4b85c40
vartanbeno <vartanbeno@gmail.com>
2018-10-02 19:56:42 -0400

    conditional output

--- src/Ninjas.js
+++ src/Ninjas.js
@@ -4,13 +4,13 @@ const Ninjas = (props) => {
     const { ninjas } = props;
 
     const listOfNinjas = ninjas.map(ninja => {
-        return (
+        return ninja.age > 24 ? (
             <div className="ninja" key={ ninja.id }>
                 <div>Name: { ninja.name }</div>
                 <div>Age: { ninja.age }</div>
                 <div>Belt: { ninja.belt }</div>
             </div>
-        )
+        ) : null;
     })
 
     return (
