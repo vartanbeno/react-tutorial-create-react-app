12ab376549486fc33e065b56a914bec964ff3468
vartanbeno <vartanbeno@gmail.com>
2018-10-02 18:59:23 -0400

    create component

---
+++ src/Ninjas.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Ninjas extends Component {
+    render() {
+        return(
+            <div className="ninja">
+                <div>Name: Vartan</div>
+                <div>Age: 23</div>
+                <div>Belt: Black</div>
+            </div>
+        );
+    }
+}
+
+export default Ninjas;
