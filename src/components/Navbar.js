195e3338152b0ac3bbeddf1db13236d1beecf35e
vartanbeno <vartanbeno@gmail.com>
2018-10-03 17:54:31 -0400

    Link and NavLink

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,24 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
+// NavLink can be used to add 'active' class to element when on that route
 const Navbar = () => {
     return (
         <nav className="navbar is-transparent">
             <div className="navbar-menu">
 
                 <div className="navbar-start">
-                    <a href="/" className="navbar-item">Home</a>
-                    <a href="/about" className="navbar-item">About</a>
-                    <a href="/contact" className="navbar-item">Contact</a>
+                    <Link to="/" className="navbar-item">Home</Link>
+                    <Link to="/about" className="navbar-item">About</Link>
+                    <Link to="/contact" className="navbar-item">Contact</Link>
                 </div>
 
                 <div className="navbar-end">
                     <div className="navbar-item has-dropdown is-hoverable">
-                        <a className="navbar-link">Stuff</a>
+                        <div className="navbar-link">Stuff</div>
                         <div className="navbar-dropdown is-boxed">
-                            <a href="/ninjas" className="navbar-item">Ninjas</a>
-                            <a href="/todos" className="navbar-item">Todos</a>
+                            <Link to="/ninjas" className="navbar-item">Ninjas</Link>
+                            <Link to="/todos" className="navbar-item">Todos</Link>
                         </div>
                     </div>
                 </div>
