da7a373d7dddf5287066599f8c149ce042622fa0
vartanbeno <vartanbeno@gmail.com>
2018-10-04 11:26:17 -0400

    get post by id with Redux

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,27 +1,14 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import { connect } from 'react-redux';
 
 class Post extends Component {
 
-    state = {
-        post: null
-    }
-
-    componentDidMount() {
-        let id = this.props.match.params.post_id;
-        axios.get('https://jsonplaceholder.typicode.com/posts/' + id).then(res => {
-            this.setState({
-                post: res.data
-            })
-        })
-    }
-
     render() {
 
-        const post = this.state.post ? (
+        const post = this.props.post ? (
             <div className="post">
-                <h1 className="has-text-centered">{ this.state.post.title }</h1>
-                <p>{ this.state.post.body }</p>
+                <h1 className="has-text-centered">{ this.props.post.title }</h1>
+                <p>{ this.props.post.body }</p>
             </div>
         ) : (
             <p className="has-text-centered">Post does not exist.</p>
@@ -37,4 +24,14 @@ class Post extends Component {
 
 }
 
-export default Post;
+const mapStateToProps = (state, ownProps) => {
+
+    let id = ownProps.match.params.post_id;
+
+    return {
+        post: state.posts.find(post => post.id === id)
+    }
+
+}
+
+export default connect(mapStateToProps)(Post);
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,8 @@
 const initState = {
     posts: [
-        { id: 1, title: 'This is a post', body: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui excepturi, iste, quasi reiciendis provident suscipit magni cumque veritatis minus ipsum sit? Iusto earum dicta perspiciatis autem non ipsa, quia eaque.' },
-        { id: 2, title: 'This is another post', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus blanditiis excepturi at, voluptatum fugit harum maxime porro repellat eos, labore, eius corporis sint ducimus! Nam sint animi culpa doloremque magnam.' },
-        { id: 3, title: 'Test post', body: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa voluptas laborum nostrum? Consectetur beatae, ipsum ratione perferendis iste impedit voluptas sapiente explicabo ducimus repellat illo ea nemo eos, repudiandae reprehenderit?' },
+        { id: '1', title: 'This is a post', body: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui excepturi, iste, quasi reiciendis provident suscipit magni cumque veritatis minus ipsum sit? Iusto earum dicta perspiciatis autem non ipsa, quia eaque.' },
+        { id: '2', title: 'This is another post', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus blanditiis excepturi at, voluptatum fugit harum maxime porro repellat eos, labore, eius corporis sint ducimus! Nam sint animi culpa doloremque magnam.' },
+        { id: '3', title: 'Test post', body: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa voluptas laborum nostrum? Consectetur beatae, ipsum ratione perferendis iste impedit voluptas sapiente explicabo ducimus repellat illo ea nemo eos, repudiandae reprehenderit?' },
     ]
 }
 
