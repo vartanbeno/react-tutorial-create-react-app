4be545f8ffd6562170c8048238c04f34748790e0
vartanbeno <vartanbeno@gmail.com>
2018-10-03 21:53:04 -0400

    Switch tag to only load one route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
@@ -25,12 +25,14 @@ class App extends Component {
         <div className="container">
           <Navbar />
           <div className="content">
-            <Route exact path="/" component={ Home } />
-            <Route path="/about" component={ About } />
-            <Route path="/contact" component={ Contact } />
-            <Route path="/ninjas" component={ NinjasPage } />
-            <Route path="/todos" component={ TodosPage } />
-            <Route path="/:post_id" component={ Post } />
+            <Switch>
+              <Route exact path="/" component={ Home } />
+              <Route path="/about" component={ About } />
+              <Route path="/contact" component={ Contact } />
+              <Route path="/ninjas" component={ NinjasPage } />
+              <Route path="/todos" component={ TodosPage } />
+              <Route path="/:post_id" component={ Post } />
+            </Switch>
           </div>
         </div>
       </BrowserRouter>
